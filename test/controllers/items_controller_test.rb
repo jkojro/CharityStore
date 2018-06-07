51c2899e77fd27446797e0c803b2be06468e822c
jkojro <janusz.kojro@gmail.com>
2018-06-07 15:39:57 +0200

    add Cart view

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create item"  do
+  	assert_difference('Item.count') do
+  		post items_url, params: {product_id: products(:product).id}
+  	end
+
+  	follow_redirect!
+
+  	assert_select 'h2', 'MÓJ KOSZYK'
+  	assert_select 'li', 'Example title'
+  end
+
 end
